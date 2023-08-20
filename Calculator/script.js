c5a94ba105fbe4e492af0fc9b0baeffba3ab3c51
bia <91140992+nabilaFajri@users.noreply.github.com>
2023-08-20 20:03:54 +0700

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,20 @@
+// script.js
+
+function appendToDisplay(value) {
+    document.getElementById('result').value += value;    
+
+}
+
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
+
